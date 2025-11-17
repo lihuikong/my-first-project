93e47a7c1e251c98917d0593f10660bc1379ad40
lihuikong <3324567099@qq.com>
2025-11-17 18:29:43 +0800

    测试

--- 变量和基本类型/类型转换.cpp
+++ 变量和基本类型/类型转换.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     bool b=42;//bool值只辨真假
     int i=b;//i的值为1
-    i=3.14;//i的值为3，因为i为int类型
+    i=3.14;//i的值为3，因为i为i·nt类型
     double pi = i;//pi也为3，因为i为3
     unsigned char c=-1;/*unsigned类型只能表示0至255区间的数
                          若是超出区间就会得到进行对256取模后的余数。*/
